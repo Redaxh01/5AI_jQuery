c76de179e25214763f08478e0ec2b1382faf3f1a
Redaelli Emanuele <redaelli.emanuele@osdbsesto>
2020-01-24 09:58:05 +0100

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-document.getElementById("h1").innerHTML = "CIAO COME STAI";
+
+var btn = document.createElement("button");
+
+var body = document.getElementsByTagName("body")[0];
+body.append(btn);
+btn.innerHTML = "CLICCA";
+btn.addEventListener("click", function(){
+    document.getElementById("h1").innerHTML = "CIAO COME STAI";
+});
